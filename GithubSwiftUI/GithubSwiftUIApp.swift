d161672e61626bfd1f9635a6a62a56054cfa6d6e
A'zamjon Abdumuxtorov <azamjonabdumuxtorov1220@gmail.com>
2024-02-23 09:09:29 +0500

    Initial Commit

---
+++ GithubSwiftUI/GithubSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubSwiftUIApp.swift
+//  GithubSwiftUI
+//
+//  Created by A'zamjon Abdumuxtorov on 23/02/24.
+//
+
+import SwiftUI
+
+@main
+struct GithubSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
